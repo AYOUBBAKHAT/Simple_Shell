b7fe3a856848332fa4c00f6200b2dd27ec6b0b82
ahmedazagaz <contactwithazagaz@gmail.com>
2023-11-09 08:31:38 -0800

    C

---
+++ main.c
@@ -0,0 +1,29 @@
+#include "shell.h"
+/**
+* main - Simple Shell main function 
+* @ac: Count of argumnents
+* @av: Arguments
+*Return: 0 Always (success).
+*/
+
+int main (int ac, char **argv)
+{
+	char *line = NULL;
+//	char **command = NULL;
+	int status = 0; 
+	(void) ac;
+
+	while (1)
+	{
+   		line = read_line();
+   		if (line == NULL) /* reache EOF ctr + D */
+   			return (status);
+   			
+   			
+   		printf("%s", line);
+//   		command = tokenizer(line);
+
+//   		status = _execute(command; argv);
+	}
+
+}
