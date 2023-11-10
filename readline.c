4aac4abbe46b7d778af514c5ec00d65c347a19b7
root <AYOUBBAKHAT>
2023-11-10 14:10:31 -0800

    C

--- readline.c
+++ readline.c
@@ -1,12 +1,12 @@
-#include "shell.h"
+include "shell.h"
 
 char *read_line(void)
 {
         char *line = NULL;
         size_t len = 0;
         ssize_t n;
-        if (isatty(STDIN_FILENO))
-        write(STDOUT_FILENO,"$ ",2);
+        if (isatty(STDIN_FILEND))
+        	write(STDOUT_FILENO,"$ ",2);
         n = getline(&line, &len, stdin);
         if (n == -1)
         {
