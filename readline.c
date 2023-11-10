b225267a473f8f2eac8aa73468ec41a19fae782c
root <AYOUBBAKHAT>
2023-11-10 14:14:53 -0800

    C

--- readline.c
+++ readline.c
@@ -5,7 +5,7 @@ char *read_line(void)
         char *line = NULL;
         size_t len = 0;
         ssize_t n;
-        if (isatty(STDIN_FILEND))
+        if (isatty(STDIN_FILENO))
         	write(STDOUT_FILENO,"$ ",2);
         n = getline(&line, &len, stdin);
         if (n == -1)
