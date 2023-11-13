e7d36ef56bad9634dc2b51f858e46aef5d551fd9
ahmedazagaz <contactwithazagaz@gmail.com>
2023-11-12 16:06:39 -0800

    C

--- main.c
+++ main.c
@@ -5,13 +5,12 @@
 * @av: Arguments
 *Return: 0 Always (success).
 */
-
 int main(int ac, char **argv)
 {
 	char *line = NULL, **command = NULL;
-	int i, status = 0; 
+	int status = 0; 
 	(void) ac;
-	(void) argv;
+	
 	
  
 	while (1)
@@ -28,11 +27,9 @@ int main(int ac, char **argv)
      	command = tokenizer(line);
      	if (!command)
      	   continue;
-     	   
-     	for (i=0; command[i]; i++)
-     	    printf("%s\n", command[i]);
-        
-	   /* status = _execute(command; argv); */
+
+     	
+	    status = _execute(command, argv); 
 	}
 
 }
