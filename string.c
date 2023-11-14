a6c4ea4da4007d4f833478982899b2c939cbba84
root <AYOUBBAKHAT>
2023-11-14 14:17:20 -0800

    C

--- string.c
+++ string.c
@@ -20,3 +20,57 @@ char *_strdup(const char *str)
                 ptr[i] = str[i];
         return (ptr);
 }
+int _strcmp(char *s1, char *s2)
+{
+        int cmp;
+
+        cmp = (int)*s1 - (int)*s2;
+        while (*s1)
+        {
+                if (*s1 != *s2)
+                        break;
+                s1++;
+                s2++;
+                cmp = (int)*s1 - (int)*s2;
+        }
+        return (cmp);
+}
+
+int _strlen(char *s)
+{
+        int len = 0;
+
+        while (s[len])
+                len++;
+        return (len);
+}
+
+char *_strcat(char *dest, char *src)
+{
+        char *p = dest;
+
+        while (*p)
+                p++;
+
+        while (*src)
+        {
+                *p = *src;
+                p++;
+                src++;
+        }
+        *p = *src;
+        return (dest);
+}
+
+char *_strcpy(char *dest, char *src)
+{
+        int i = 0;
+
+        while (src[i])
+        {
+                dest[i] = src[i];
+                i++;
+
+        }
+        return (dest);
+}
