6c3f335f2692ba0216a3cd0a723532218f9c93d8
ahmedazagaz <contactwithazagaz@gmail.com>
2023-11-15 15:09:50 -0800

    C

--- tools.c
+++ tools.c
@@ -1,9 +1,7 @@
 #include "shell.h"
-
 /**
  * freearray2D - Frees a 2D array of characters
  * @array: The 2D array to be freed
- *
  * Return: Nothing
  */
 void freearray2D(char **array)
@@ -21,7 +19,6 @@ void freearray2D(char **array)
 	free(array);
 	array = NULL;
 }
-
 /**
  * print_error - Prints an error message to the standard error output
  * @name: The name to be printed in the error message
@@ -47,9 +44,8 @@ void print_error(char *name, char *cmd, int idx)
 /**
  * _itoa - Converts an integer to a string
  * @n: The integer to be converted. This is the integer that will be converted to a string.
- *
  * This function first checks if the number is zero, if it is, it simply assigns '0'
- * to the first position of the buffer. If the number is not zero, it converts the
+ * * to the first position of the buffer. If the number is not zero, it converts the
  * number to a string by repeatedly dividing the number by 10 and storing the
  * remainder in the buffer. After the conversion, it reverses the string to get
  * the correct order.
@@ -76,16 +72,10 @@ char *_itoa(int n)
 
 	return (_strdup(buffer));
 }
-
 /**
  * reverse_string - Reverses a string
  * @str: The string to be reversed. This is the string that will be reversed.
  * @len: The length of the string. This is the length of the string to be reversed.
- *
- * This function reverses a string by swapping the characters at the start
- * and end of the string and moving towards the center of the string.
- *
- * Return: Nothing
  */
 void reverse_string(char *str, int len)
 {
