edf73a00aeb3dbfda1380dc1349e3faad6256192
root <AYOUBBAKHAT>
2023-11-14 17:33:31 -0800

    C

--- execute.c
+++ execute.c
@@ -1,15 +1,15 @@
 #include "shell.h"
 
-int _execute(char **command, char **argv)
+int _execute(char **command, char **argv, int idx)
 {
-	char *fullcmd;
+	char *full_cmd;
     pid_t child;
     int status;
     
 	full_cmd = _getpath(command[0]);
 	if (!full_cmd)
 	{
-		printerror(argv[0], command[0], idx);
+		print_error(argv[0], command[0], idx);
 		freearray2D(command);
 		return (127);
 	}
@@ -18,7 +18,7 @@ int _execute(char **command, char **argv)
     	{
         	if (execve(command[0], command, environ) == -1)
         	{
-        		free(full_cmd), full_cmd = Null;
+        		free(full_cmd), full_cmd = NULL;
         		freearray2D(command);
         	}
     	}
