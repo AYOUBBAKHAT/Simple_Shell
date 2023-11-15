fa3b6f61336e1717b5df2f4c5a383f52011f5581
ahmedazagaz <contactwithazagaz@gmail.com>
2023-11-15 05:10:32 -0800

    C

--- tools.c
+++ tools.c
@@ -1,4 +1,5 @@
 #include "shell.h"
+
 void freearray2D(char **array)
 {
     int i;
@@ -17,56 +18,54 @@ void freearray2D(char **array)
 
 void print_error(char *name, char *cmd, int idx)
 {
-  char *index, mssg[] = ": not found\n";
+    char *index, mssg[] = ": not found\n";
   
-  index = _itoa(idx);
-  
-  /*write(STDERR_FILENO, char*str,int len)*/
-  write (STDERR_FILENO, name, _strlen(name));
-  write (STDERR_FILENO, ": ", 2);
-  write (STDERR_FILENO, index, _strlen(index));
-  write (STDERR_FILENO, ": ", 2);
-  write (STDERR_FILENO, cmd, _strlen(cmd));
-  write (STDERR_FILENO, mssg, _strlen(mssg));
-  
-  free(index);
+    index = _itoa(idx);
+
+    /* write(STDERR_FILENO, char*str,int len) */
+    write(STDERR_FILENO, name, _strlen(name));
+    write(STDERR_FILENO, ": ", 2);
+    write(STDERR_FILENO, index, _strlen(index));
+    write(STDERR_FILENO, ": ", 2);
+    write(STDERR_FILENO, cmd, _strlen(cmd));
+    write(STDERR_FILENO, mssg, _strlen(mssg));
+
+    free(index);
 }
-char * _itoa(int n)
+
+char *_itoa(int n)
 {
-	char buffer[20];
-	int i = 0;
-	
-	if (n == 0)
-		buffer[i++] = '0';
-	else
-	{
-		while (n > 0)
-		{
-			buffer[i++] = (n % 10) + '0';
-			n /= 10;
-		}
-		
-	}
-	buffer[i] = '\0';
-	reverse_string(buffer, i);
-	
-	return (_strdup(buffer));	
+    char buffer[20];
+    int i = 0;
+  
+    if (n == 0)
+        buffer[i++] = '0';
+    else
+    {
+        while (n > 0)
+        {
+            buffer[i++] = (n % 10) + '0';
+            n /= 10;
+        }
+    }
+    buffer[i] = '\0';
+    reverse_string(buffer, i);
+  
+    return (_strdup(buffer));
 }
 
 void reverse_string(char *str, int len)
 {
-	
-	char tmp;
-	int start = 0;
-	int end = len -1;
-	
-	while (start < end)
-	{
-		tmp = str[start];
-		str[end] = tmp;
-		start++;
-		end--;
-	}
+    char tmp;
+    int start = 0;
+    int end = len - 1;
 
+    while (start < end)
+    {
+        tmp = str[start];
+        str[end] = tmp;
+        start++;
+        end--;
+    }
 }
 
