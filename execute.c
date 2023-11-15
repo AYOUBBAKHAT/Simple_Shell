76ef6c209697d2821f54c5565b4d9dcc28521e7b
root <AYOUBBAKHAT>
2023-11-14 17:25:20 -0800

    C

--- execute.c
+++ execute.c
@@ -1,23 +1,32 @@
 #include "shell.h"
+
 int _execute(char **command, char **argv)
 {
-        pid_t child;
-        int status;
-
+	char *fullcmd;
+    pid_t child;
+    int status;
+    
+	full_cmd = _getpath(command[0]);
+	if (!full_cmd)
+	{
+		printerror(argv[0], command[0], idx);
+		freearray2D(command);
+		return (127);
+	}
 	child = fork();
     	if (child == 0)
     	{
         	if (execve(command[0], command, environ) == -1)
         	{
-        		perror(argv[0]);
+        		free(full_cmd), full_cmd = Null;
         		freearray2D(command);
-			exit(0);
         	}
     	}
     	else
     	{
         	waitpid(child, &status, 0);
         	freearray2D(command);
+        	free(full_cmd), full_cmd = Null;
     	}
-    	return(WEXITSTATUS(status));
+    	return (WEXITSTATUS(status));
 }
--- handle_path.c
+++ handle_path.c
@@ -2,14 +2,24 @@
 char *_getpath(char *command)
 {
 	char *path_env, *full_cmd, *dir;
+	int i;
 	struct stat st;
 	
+	for (i = 0; command[i]; i++)
+	{
+		if (command[i] == '/')
+		{
+			if (stat(command, &st) == 0)
+				return (_strdup(command));
+			return (NUll);
+		}
+	}
 	path_env = _getenv("PATH");
-	
+	if (!path_env)
+		return (NULL);
 	dir = strtok(path_env, ":");
 	while (dir)
 	{
-		/* size = len(directory) + len(commad) + 2 ('/' and '\0') */
 		full_cmd = malloc(_strlen(dir) + _strlen(command) + 2);
 		if (full_cmd)
 		{
@@ -22,20 +32,9 @@ char *_getpath(char *command)
 				return (full_cmd);
 			}
 			free(full_cmd), full_cmd = NULL;
-			
 			dir = strtok(NULL, ":");
 		}
 	}
 	free(path_env);
 	return (NULL);
-}
-int main(int ac, char **av)
-{
-	char *full_cmd;
-	
-	full_cmd = _getpath(av[1]);
-	if (full_cmd)
-		printf("%s\n", full_cmd);
-	else
-		printf("does not exist");
 }
