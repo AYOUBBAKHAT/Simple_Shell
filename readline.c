2a9bbbac104173c867e8fe23833b866edf3a9577
root <AYOUBBAKHAT>
2023-11-15 12:58:14 -0800

    C

--- readline.c
+++ readline.c
@@ -1,14 +1,5 @@
 #include "shell.h"
 
-/*
- * read_line - Read a line from standard input.
- *
- * This function reads a line from the standard input and returns it as a
- * dynamically allocated string.
- *
- * Return: A pointer to the allocated string containing the read line.
- * If an error occurs or the end of the file is reached, NULL is returned.
- */
 char *read_line(void)
 {
 	char *line = NULL;
@@ -16,14 +7,12 @@ char *read_line(void)
 	ssize_t n;
 
 	if (isatty(STDIN_FILENO))
-		write(STDOUT_FILENO, "$ ", 2);
-
+		write(STDOUT_FILENO,"$ ",2);
 	n = getline(&line, &len, stdin);
 	if (n == -1)
 	{
-		free(line);
-		return NULL;
-	}
-
+	free(line);
+	return (NULL);
+    	}
 	return (line);
 }
