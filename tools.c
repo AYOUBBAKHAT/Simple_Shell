8e126bbb18309bc1b48ea830efd2ac3b8e0ec6a7
ahmedazagaz <contactwithazagaz@gmail.com>
2023-11-15 06:09:20 -0800

    C

--- tools.c
+++ tools.c
@@ -1,5 +1,9 @@
 #include "shell.h"
 
+/**
+ * freearray2D - Frees a two-dimensional array of strings.
+ * @array: The array to be freed.
+ */
 void freearray2D(char **array)
 {
     int i;
@@ -16,13 +20,18 @@ void freearray2D(char **array)
     array = NULL;
 }
 
+/**
+ * print_error - Prints an error message to standard error.
+ * @name: The program name.
+ * @cmd: The command that caused the error.
+ * @idx: The index of the command.
+ */
 void print_error(char *name, char *cmd, int idx)
 {
     char *index, mssg[] = ": not found\n";
 
     index = _itoa(idx);
 
-    /* write(STDERR_FILENO, char*str,int len) */
     write(STDERR_FILENO, name, _strlen(name));
     write(STDERR_FILENO, ": ", 2);
     write(STDERR_FILENO, index, _strlen(index));
@@ -33,6 +42,12 @@ void print_error(char *name, char *cmd, int idx)
     free(index);
 }
 
+/**
+ * _itoa - Converts an integer to a string.
+ * @n: The integer to be converted.
+ *
+ * Return: The resulting string.
+ */
 char *_itoa(int n)
 {
     char buffer[20];
@@ -54,6 +69,11 @@ char *_itoa(int n)
     return (_strdup(buffer));
 }
 
+/**
+ * reverse_string - Reverses a string.
+ * @str: The string to be reversed.
+ * @len: The length of the string.
+ */
 void reverse_string(char *str, int len)
 {
     char tmp;
