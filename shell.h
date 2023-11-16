dc03b25a6bf341922333a7fbf6b1192b8016bbf7
root <AYOUBBAKHAT>
2023-11-15 17:32:23 -0800

    C

--- shell.h
+++ shell.h
@@ -19,6 +19,10 @@ int _execute(char **command, char **argv, int idx);
 char *_getenv(char *variable);
 char *_getpath(char *command);
 
+void handle_builtin(char **command, char **argv, int status, int idx);
+void exit_shell(char **command, int status);
+void print_env (char **command, int status)
+
 char *_strdup(const char *str);
 int _strcmp(char *s1, char *s2);
 int _strlen(char *s);
