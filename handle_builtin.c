190ed1edf78a21c7d5b5a6d969f05326ed242524
ahmedazagaz <contactwithazagaz@gmail.com>
2023-11-16 05:30:11 -0800

    C

--- handle_builtin.c
+++ handle_builtin.c
@@ -8,9 +8,9 @@ int is_builtin(char *command)
 	};
 	int i;
 
-	for (i=0; builtins[i]; i++) 
+	for (i = 0; builtins[i]; i++) 
 	{
-		if (_strcmp (command, builtins[i]) == 0)
+		if (_strcmp(command, builtins[i]) == 0)
 			return (1);
 	}
 	return (0);
@@ -19,6 +19,7 @@ int is_builtin(char *command)
 void handle_builtin(char **command, char **argv, int *status, int idx)
 {
 
+	
 	if (_strcmp(command[0], "exit") == 0)
 	    exit_shell(command, argv, status, idx);
 	
@@ -27,27 +28,29 @@ void handle_builtin(char **command, char **argv, int *status, int idx)
 }
 
 void exit_shell(char **command, char **argv, int *status, int idx)
-{
+{   
 	int exit_value = (*status);
-	char *index, mssg[] =":exit: Illegal number:";
+	char *index, mssg[]= ": exit Illegal number:";
 	
 	if (command[1])
 	{
 		if (is_positive_number(command[1]))
 		{
-			exit_value = _atoi(command[1]);
+		exit_value = _atoi(command[1]);
 		}
 		else
 		{
+			
 		    index = _itoa(idx);
-		    write(STDERR_FILENO, argv[0], _strlen(argv[0]));
-		    write(STDERR_FILENO, ": ", 2);
-		    write(STDERR_FILENO, index, _strlen(index));
-		    write(STDERR_FILENO, mssg, _strlen(mssg));
-		    write(STDERR_FILENO, command[1], _strlen(command[1]));
-		    write(STDERR_FILENO, "\n", 1);
-		    free(index);
-		    freearray2D(command);
+            write(STDERR_FILENO, argv[0], _strlen(argv[0]));
+            write(STDERR_FILENO, ": ", 2);
+            write(STDERR_FILENO, index, _strlen(index));
+            write(STDERR_FILENO, mssg, _strlen(mssg));
+            write(STDERR_FILENO, command[1], _strlen(command[1]));
+            write(STDERR_FILENO, "\n", 1);
+            free(index);
+            freearray2D(command);
+                
 			return;
 		}
 	}
@@ -57,8 +60,8 @@ void exit_shell(char **command, char **argv, int *status, int idx)
 
 void print_env(char **command, int *status)
 {
-	int i;
-	
+ 	 int i;
+		
 	for (i = 0; environ[i]; i++)
 	{
 		write(STDOUT_FILENO, environ[i], _strlen(environ[i]));
@@ -67,4 +70,5 @@ void print_env(char **command, int *status)
 	freearray2D(command);
 	*status = 0;
 }
+
 
