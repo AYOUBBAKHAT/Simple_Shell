2eccb5de39f8facefefe55dfd14b7e13c1f835a8
root <AYOUBBAKHAT>
2023-11-15 18:02:55 -0800

    C

--- shell.h
+++ shell.h
@@ -22,6 +22,7 @@ char *_getpath(char *command);
 void handle_builtin(char **command, char **argv, int status, int idx);
 void exit_shell(char **command, int status);
 void print_env(char **command, int status);
+int is_builtin(char *command);
 
 char *_strdup(const char *str);
 int _strcmp(char *s1, char *s2);
